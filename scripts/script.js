dd7c5bbe8c2d52e20074a79c0f33f305b432ffaa
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-05-21 20:51:03 +0300

    Adding listeners to start and end divisions

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,8 @@ window.addEventListener("load", function(){
 
     let end_div = document.getElementById("end");
 
+    let cheating_detector_div = document.getElementById("game");
+
     let status = document.getElementById("status");
 
     let i;
@@ -16,4 +18,39 @@ window.addEventListener("load", function(){
     var score = 0;  //global variables
 
 
-});
+/////Main
+
+    startGame();
+
+
+/////Functions
+
+    //activate start div
+
+    function startGame() {
+
+        start_div.addEventListener("mouseover", start);
+
+    }
+    //deactivate start div
+
+    function endGame() {
+
+        start_div.removeEventListener("mouseover", start);
+
+    }
+
+    function activateEndDiv() {
+
+        end_div.addEventListener("mouseover", win);
+
+    }
+
+    function deactivateEndDiv() {
+
+        end_div.removeEventListener("mouseover", win);
+
+    }
+
+
+    });
