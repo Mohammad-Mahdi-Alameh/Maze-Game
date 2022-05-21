1bd7b4034e8d1d4ec5e50a31ed9c41cbe0cb5dc5
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-05-21 20:44:23 +0300

    Initiazling variables needed

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", function(){
+
+    /////Declaring Variables
+
+
+    let lose_div = document.getElementsByClassName("boundary");
+
+    let start_div = document.getElementById("start");
+
+    let end_div = document.getElementById("end");
+
+    let status = document.getElementById("status");
+
+    let i;
+
+    var score = 0;  //global variables
+
+
+});
